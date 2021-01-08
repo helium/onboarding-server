da89650bd22caa5edd58bf3e099dcca63986fa6c
Andrew Allen <allenan@helium.com>
2021-01-08 15:14:26 -0800

    makers model

---
+++ src/migrations/20210108180313-create-maker.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Makers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      apiKey: {
+        type: Sequelize.STRING,
+      },
+      locationNonceLimit: {
+        allowNull: false,
+        defaultValue: 1,
+        type: Sequelize.INTEGER,
+      },
+      keypair: {
+        type: Sequelize.STRING.BINARY,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Makers')
+  },
+}
---
+++ src/models/maker.js
@@ -0,0 +1,28 @@
+const { Model } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class Maker extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Maker.init(
+    {
+      name: DataTypes.STRING,
+      address: DataTypes.STRING,
+      apiKey: DataTypes.STRING,
+      locationNonceLimit: DataTypes.INTEGER,
+      keypair: DataTypes.STRING.BINARY,
+    },
+    {
+      sequelize,
+      modelName: 'Maker',
+    },
+  )
+  return Maker
+}
