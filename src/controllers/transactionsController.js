cf4f0ccab71381bca0a2a2806f250d7359275338
Andrew Allen <allenan@helium.com>
2021-01-11 12:24:17 -0800

    add support for signing both add gw and assert loc txns

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,31 +1,42 @@
 import { Keypair } from '@helium/crypto'
-import { AddGatewayV1, AssertLocationV1, PaymentV2 } from '@helium/transactions'
+import {
+  Transaction,
+  AddGatewayV1,
+  AssertLocationV1,
+} from '@helium/transactions'
 import { Maker, Hotspot } from '../models'
 import { errorResponse, successResponse } from '../helpers'
-
-const deserializeTxn = (txnString) => {
-  // TODO figure out what kind it is
-  const txn = AddGatewayV1.fromString(txnString)
-  return txn
-}
 
 export const pay = async (req, res) => {
   try {
     const { onboardingKey } = req.params
     const { transaction } = req.body
 
-    const hotspot = await Hotspot.findOne({
+    const { Maker: maker } = await Hotspot.findOne({
       where: { onboardingKey },
       include: Maker.scope('withKeypair'),
     })
-    const maker = hotspot.Maker
     const keypairEntropy = Buffer.from(maker.keypairEntropy, 'hex')
     const keypair = await Keypair.fromEntropy(keypairEntropy)
 
-    const txn = deserializeTxn(transaction)
-    // TODO: if location, validate nonce
-    const signedTxn = await txn.sign({ payer: keypair })
+    let txn
+    switch (Transaction.stringType(transaction)) {
+      case 'addGateway':
+        txn = AddGatewayV1.fromString(transaction)
+        break
 
+      case 'assertLocation':
+        txn = AssertLocationV1.fromString(transaction)
+        if (txn.nonce > maker.locationNonceLimit) {
+          return errorResponse(req, res, 'Nonce limit exceeded', 422)
+        }
+        break
+
+      default:
+        throw new Error('Unsupported transaction type')
+    }
+
+    const signedTxn = await txn.sign({ payer: keypair })
     return successResponse(req, res, { txn: signedTxn.toString() })
   } catch (error) {
     errorResponse(req, res, error.message, 500, error.errors)
