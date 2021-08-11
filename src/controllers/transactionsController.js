7ef2149c95c05d1610cfbc35123a9c1962f957b6
Andrew Allen <allenan@helium.com>
2021-08-11 10:58:34 -0700

    validate gateway address (#2)
    * validate gateway address
    
    * add some comments and additional sanity checks
    
    * add error response
    
    * change message wording

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -9,14 +9,25 @@ import { Maker, Hotspot } from '../models'
 import { errorResponse, successResponse } from '../helpers'
 import { Op } from 'sequelize'
 
+const env = process.env.NODE_ENV || 'development'
+
 export const pay = async (req, res) => {
   try {
     const { onboardingKey } = req.params
     const { transaction } = req.body
 
+    if (!transaction) {
+      return errorResponse(req, res, 'Missing transaction param', 422)
+    }
+
     const hotspot = await Hotspot.findOne({
       where: { [Op.or]: [{ onboardingKey }, { publicAddress: onboardingKey }] },
     })
+
+    if (!hotspot) {
+      return errorResponse(req, res, 'Hotspot not found', 404)
+    }
+
     const maker = await Maker.scope('withKeypair').findByPk(hotspot.makerId)
     const keypairEntropy = Buffer.from(maker.keypairEntropy, 'hex')
     const keypair = await Keypair.fromEntropy(keypairEntropy)
@@ -29,6 +40,8 @@ export const pay = async (req, res) => {
 
       case 'assertLocation':
         txn = AssertLocationV1.fromString(transaction)
+
+        // transactions are only signed up until the maker's nonce limit
         if (txn.nonce > maker.locationNonceLimit) {
           return errorResponse(req, res, 'Nonce limit exceeded', 422)
         }
@@ -36,6 +49,8 @@ export const pay = async (req, res) => {
 
       case 'assertLocationV2':
         txn = AssertLocationV2.fromString(transaction)
+
+        // transactions are only signed up until the maker's nonce limit
         if (txn.nonce > maker.locationNonceLimit) {
           return errorResponse(req, res, 'Nonce limit exceeded', 422)
         }
@@ -45,10 +60,19 @@ export const pay = async (req, res) => {
         throw new Error('Unsupported transaction type')
     }
 
+    // The transaction must include the onboarding server as the payer
     if (txn?.payer?.b58 !== maker.address) {
       return errorResponse(req, res, 'Invalid payer address', 422)
     }
 
+    // Starting after hotspot 32951, it's required that the onboarding key
+    // match the txn gateway address
+    if (hotspot.id > 32951 && txn?.gateway?.b58 !== onboardingKey) {
+      return errorResponse(req, res, 'Invalid hotspot address', 422)
+    }
+
+    // Once an onboarding key has been associated with a hotspot's public
+    // address, it cannot be used for a hotspot with a different public address
     if (hotspot.publicAddress && hotspot.publicAddress !== txn?.gateway?.b58) {
       return errorResponse(req, res, 'Onboarding key already used', 422)
     }
@@ -59,13 +83,22 @@ export const pay = async (req, res) => {
     const signedTxn = await txn.sign({ payer: keypair })
     return successResponse(req, res, { transaction: signedTxn.toString() })
   } catch (error) {
-    errorResponse(req, res, error.message, 500, error.errors)
+    errorResponse(
+      req,
+      res,
+      env === 'development' ? error.message : 'Internal error',
+      500,
+      env === 'development' ? error.errors : [],
+    )
   }
 }
 
-// TODO: delete below, just for testing
 export const sample = async (req, res) => {
-  const maker = await Maker.findByPk(3)
+  if (env === 'production') {
+    return errorResponse(req, res, 'Not available', 422)
+  }
+
+  const maker = await Maker.scope('withKeypair').findByPk(1)
   const keypairEntropy = Buffer.from(maker.keypairEntropy, 'hex')
   const keypair = await Keypair.fromEntropy(keypairEntropy)
 
