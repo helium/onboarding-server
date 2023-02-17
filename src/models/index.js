031b92c81d7e1aebb934cb84f7257c85d7af439b
Chewing Glass <chewinglass123@gmail.com>
2023-02-17 15:29:03 -0600

    Switch to aws token auth and env vars

--- src/models/index.js
+++ src/models/index.js
@@ -2,25 +2,51 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
 const db = {}
+const AWS = require('aws-sdk')
+const pg = require('pg')
 
-if (process.env.DISABLE_DB_LOGGING === 'true') {
-  config.logging = false
-}
+const host = process.env.PGHOST || 'localhost'
+const port = Number(process.env.PGPORT) || 5432
+const sequelize = new Sequelize({
+  host: host,
+  dialect: 'postgres',
+  port: port,
+  logging: process.env.DISABLE_DB_LOGGING !== 'true',
+  dialectModule: pg,
+  username: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  hooks: {
+    beforeConnect: async (config) => {
+      const isRds = host.includes('rds.amazonaws.com')
 
-let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  )
-}
+      let password = process.env.PGPASSWORD
+      if (isRds && !password) {
+        const signer = new AWS.RDS.Signer({
+          region: process.env.AWS_REGION,
+          hostname: process.env.PGHOST,
+          port,
+          username: process.env.PGUSER,
+        })
+        password = await new Promise((resolve, reject) =>
+          signer.getAuthToken({}, (err, token) => {
+            if (err) {
+              return reject(err)
+            }
+            resolve(token)
+          }),
+        )
+        config.dialectOptions = {
+          ssl: {
+            require: false,
+            rejectUnauthorized: false,
+          },
+        }
+      }
+      config.password = password
+    },
+  },
+})
 
 fs.readdirSync(__dirname)
   .filter((file) => {
